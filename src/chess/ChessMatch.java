3232ce726f8faaefcb9e2439b19232c53601355b
Flavio Nunes <flavionbueri@gmail.com>
2022-07-29 06:28:17 -0300

    Tratando melhor a promoção

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -131,7 +130,7 @@ public class ChessMatch {
 		   throw new IllegalStateException("Nao ha peca para ser promovida.");		   
 	   }
 	   if(!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
-		   throw new InvalidParameterException("Tipo de promocao invalida.");
+		   return promoted;
 	   }
 	   
 	   Position pos = promoted.getChessPosition().toPosition();
